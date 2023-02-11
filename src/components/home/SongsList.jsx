58dda574300d8f5946628c1ae364b119b2c06540
fmoraless <franticzero@gmail.com>
2023-02-10 21:17:06 -0300

    chore(core-app): Base components

---
+++ src/components/home/SongsList.jsx
@@ -0,0 +1,14 @@
+const SongsList = () => {
+  return (
+    <div>
+      <h1>Songs Component</h1>
+      <ul>
+        <li>Song 1</li>
+        <li>Song 2</li>
+        <li>Song 3</li>
+      </ul>
+    </div>
+  )
+}
+
+export default SongsList
---
+++ src/components/login/Login.jsx
@@ -0,0 +1,11 @@
+const Login = () => {
+  return (
+    <>
+      <h3>Bienvenido</h3>
+      <h6>Inicia sesión para encontrar tu musica</h6>
+      <button>Iniciar sesión</button>
+    </>
+  )
+}
+
+export default Login
