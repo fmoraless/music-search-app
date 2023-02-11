7cc8903a052813631aeb8858db7de6ac36148a8e
fmoraless <franticzero@gmail.com>
2023-02-10 22:39:32 -0300

    Adds commit linter

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,21 +1,26 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
-    node: true,
-  },
-  extends: ["plugin:react/recommended", "standard", "prettier"],
-  overrides: [],
-  parserOptions: {
-    ecmaFeatures: {
-      jsx: true,
+    env: {
+        browser: true,
+        es2021: true,
+        node: true,
     },
-    ecmaVersion: "latest",
-    sourceType: "module",
-  },
-  plugins: ["react"],
-  rules: {
-    "react/prop-types": "off",
-    "react/react-in-jsx-scope": "off",
-  },
+    extends: ["plugin:react/recommended", "standard", "prettier"],
+    overrides: [],
+    parserOptions: {
+        ecmaFeatures: {
+            jsx: true,
+        },
+        ecmaVersion: "latest",
+        sourceType: "module",
+    },
+    plugins: ["react"],
+    settings: {
+        "react": {
+            "version": "detect"
+        },
+    },
+    rules: {
+        "react/prop-types": "off",
+        "react/react-in-jsx-scope": "off",
+    },
 }
--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,17 @@ import "./App.css"
 import Login from "./components/login/Login"
 
 function App() {
-    return (
-        <div className="main-cointainer">
-            <div className="main-left-child">
-                <Login/>
-            </div>
-            <div className="main-rigth-child">
-                <div className="url('./assets/images')">
-
-                </div>
-            </div>
-        </div>
-    )
+  const a = "Hola"
+  return (
+    <div className="main-cointainer">
+      <div className="main-left-child">
+        <Login />
+      </div>
+      <div className="main-rigth-child">
+        <div className="url('./assets/images')"></div>
+      </div>
+    </div>
+  )
 }
 
 export default App
