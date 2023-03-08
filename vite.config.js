1697517aed7d016169550879deb98b66e7aa3cdf
fmoraless <franticzero@gmail.com>
2023-03-08 13:58:36 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,25 @@ import react from "@vitejs/plugin-react"
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 3000,
+  },
+  preview: {
+    port: 3001,
+  },
+  build: {
+    incremental: true,
+
+    babel: {
+      presets: ["@babel/preset-react", "@babel/preset-env"],
+    },
+    // Modo debug para ejecuciones de la generacion del build
+    debug: true,
+    cache: true,
+    minify: true,
+    mode: "production",
+    chunks: true,
+    moduleBuilding: true,
+    devCode: true,
+  },
 })
